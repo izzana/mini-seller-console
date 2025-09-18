502566221c64182acc3a8c62c7cb369d1105c076
Izzana Barbosa Martins <izzana.martins@gmail.com>
2025-09-17 22:45:16 -0300

    feat: create Loading component

---
+++ src/components/UI/Loading.tsx
@@ -0,0 +1,12 @@
+import React, { type JSX } from 'react';
+
+function Loading(): JSX.Element {
+    return (
+      <div className="py-16 text-center text-sm text-gray-500">
+        <div className="loader"></div>
+        <span className="ml-2">Loading...</span>
+      </div>
+    );
+};
+
+export default Loading;
